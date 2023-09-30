8b87eba0f3c85641302484b5ea61b727ff9c2a88
Afnan Khalid <afnank19@gmail.com>
2023-09-30 22:18:30 +0500

    second commint

--- hehe.java
+++ hehe.java
@@ -1,5 +1,6 @@
 public class hehe{
     public static void main(String[] args) {
         System.out.println("First time using git and github");
+        System.out.println("Merged with main repo! :)");
     }
 }
